5e1d42d71c4f276f9a8d5fad3201adbe6aff2372
y-okagawa <yuya.okagawa@gmail.com>
2018-08-02 20:40:59 +0900

    add Typescript configuration

---
+++ app/components/TypeScriptSample.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ getMessage() }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    export default class TypeScriptSample extends Vue {
+      message: string;
+      constructor() {
+        super();
+        this.message = "okagaway";
+      }
+
+      getMessage(): string {
+        return this.message;
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
